534af913549712fd7a5f9c4c16ae314038e52567
Juan Renato Urrea Ortiz <UsuaryRenato@hotmail.com>
2025-01-05 21:39:48 -0800

    completed the concept of CSM Package Landing

--- lib/csm_view.dart
+++ lib/csm_view.dart
@@ -8,10 +8,14 @@ export 'src/adaptive/bases/csm_adaptive_component_base.dart';
 export 'src/adaptive/csm_adaptive_component.dart';
 export 'src/adaptive/csm_adaptive_property.dart';
 
-
 export 'src/common/common_module.dart';
 export 'src/components/widgets_module.dart';
 export 'src/csm_application.dart';
+
+/// --> Landing concept exports
+export 'src/landing/csm_package_landing.dart';
+export 'src/landing/csm_package_landing_entry.dart';
+
 export 'src/router/router_module.dart';
 export 'src/theme/theme_module.dart';
 
