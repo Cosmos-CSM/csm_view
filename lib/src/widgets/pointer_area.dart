f1ae9d76644384062d525d51ad0805ed7d552fc5
Juan Renato Urrea Ortiz <UsuaryRenato@hotmail.com>
2025-06-08 19:11:55 -0700

    adding extra properties for PointerArea

--- lib/src/widgets/pointer_area.dart
+++ lib/src/widgets/pointer_area.dart
@@ -22,19 +22,50 @@ final class PointerArea extends StatelessWidget {
   /// [Widget] to wrap and handle [PointerHandler] events.
   final Widget? child;
 
+  /// Whether this widget should prevent other [MouseRegion]s visually behind it
+  /// from detecting the pointer.
+  ///
+  /// This changes the list of regions that a pointer hovers, thus affecting how
+  /// their [onHover], [onEnter], [onExit], and [cursor] behave.
+  ///
+  /// If [opaque] is true, this widget will absorb the mouse pointer and
+  /// prevent this widget's siblings (or any other widgets that are not
+  /// ancestors or descendants of this widget) from detecting the mouse
+  /// pointer even when the pointer is within their areas.
+  ///
+  /// If [opaque] is false, this object will not affect how [MouseRegion]s
+  /// behind it behave, which will detect the mouse pointer as long as the
+  /// pointer is within their areas.
+  ///
+  /// This defaults to true.
+  final bool opaque;
+
+  /// How this gesture detector should behave during hit testing when deciding
+  /// how the hit test propagates to children and whether to consider targets
+  /// behind this one.
+  ///
+  /// This defaults to [HitTestBehavior.deferToChild] if [child] is not null and
+  /// [HitTestBehavior.translucent] if child is null.
+  ///
+  /// See [HitTestBehavior] for the allowed values and their meanings.
+  final HitTestBehavior? behavior;
+
   /// Generates a new [PointerArea] widget.
   const PointerArea({
     super.key,
-    this.cursor = MouseCursor.defer,
     this.onClick,
     this.onHover,
     this.child,
+    this.behavior,
+    this.opaque = true,
+    this.cursor = MouseCursor.defer,
   });
 
   @override
   Widget build(BuildContext context) {
     return MouseRegion(
       cursor: cursor,
+      opaque: opaque,
       onEnter: onHover != null
           ? (PointerEnterEvent event) {
               onHover?.call(true);
@@ -48,6 +79,7 @@ final class PointerArea extends StatelessWidget {
       child: GestureDetector(
         onTap: onClick,
         child: child,
+        behavior: behavior,
       ),
     );
   }
