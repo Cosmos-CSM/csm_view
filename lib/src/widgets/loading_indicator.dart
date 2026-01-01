3bd7ab59f07fd2b04d7bee350e256e2cd6fd3aa5
Juan Renato Urrea Ortiz <UsuaryRenato@hotmail.com>
2026-01-01 04:11:44 -0800

    corrected loading progress

--- lib/src/widgets/loading_indicator.dart
+++ lib/src/widgets/loading_indicator.dart
@@ -10,6 +10,9 @@ class LoadingIndicator extends StatelessWidget {
   /// Boxfit behavior.
   final BoxFit fit;
 
+  /// Whether the loading indicator just displays the circular progress indicator and not the text hint. 
+  final bool hideTextHint;
+
   /// Padding.
   final EdgeInsets padding;
   const LoadingIndicator({
@@ -19,6 +22,7 @@ class LoadingIndicator extends StatelessWidget {
       horizontal: 18,
       vertical: 8,
     ),
+    this.hideTextHint = false,
     this.fit = BoxFit.fitHeight,
   });
 
@@ -36,6 +40,8 @@ class LoadingIndicator extends StatelessWidget {
               backgroundColor: Colors.transparent,
               color: foreColor,
             ),
+
+            if (!hideTextHint)
             Text(
               'Loading...',
               style: TextStyle(
