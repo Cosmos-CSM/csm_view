535d3b1ba7123eb1dc592ea8691f3d65c5a22b89
Juan Renato Urrea Ortiz <UsuaryRenato@hotmail.com>
2026-01-06 11:42:17 -0800

    exporting

--- lib/src/abstractions/abstractions_module.dart
+++ lib/src/abstractions/abstractions_module.dart
@@ -1,9 +1,9 @@
 //! --> Exporting [bases]
+export 'bases/category_entity_view_page_base.dart';
 export 'bases/entity_view_page_base.dart';
 export 'bases/view_layout_base.dart';
 export 'bases/view_module_base.dart';
 export 'bases/view_page_base.dart';
-
 //! --> Exporting [interfaces]
 export 'interfaces/ientity_view_page.dart';
 export 'interfaces/iview_layout.dart';
